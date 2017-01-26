7b20ff6abab7cb954595591c535ff59aefc482f5
Living Jiang <694771790@qq.com>
2017-01-26 20:14:34 +0800

    Perfect pagination

--- app/list/list.js
+++ app/list/list.js
@@ -17,40 +17,55 @@ angular.module('Movie.list', ['ngRoute', "Movie.Service"])
     'AppConfig',
     '$route',
     '$routeParams',
-    function($scope, service, AppConfig,$route,$routeParams) {
+    function($scope, service, AppConfig, $route, $routeParams) {
+
         $scope.datas = {
             title: "loading...",
             start: 0
         };
+        //分页展示的页数
+        $scope.showPageCount = 5;
+        //请求数据的总条数
         $scope.total = 0;
+        //当前页码数
         $scope.curPage = 0;
+        // 每页展示电影数据的条数
         $scope.showCount = AppConfig.pageSize;
+        // 总页数
         $scope.page = Math.ceil($scope.total / $scope.showCount);
-        $scope.isLoading = true;
-        $scope.detail = AppConfig.detail;
+        $scope.isLoading = true; // 是否为加载状态
+        $scope.detail = AppConfig.detail; //进入具体电影页面的route配置
+
         $scope.changePage = function(page) {
-            if (page < 0) return;
-            if (page >= $scope.page) {
-                 return;
+                if (page < 0) return;
+                if (page >= $scope.page) {
+                    return;
+                }
+                $route.updateParams({ page: page });
             }
-            $route.updateParams({page:page});
-        }
-        // https://api.douban.com/v2/movie/search?q=张艺谋
+            // https://api.douban.com/v2/movie/search?q=张艺谋
         service.jsonp(AppConfig.listUrl + $routeParams.category, {
                 count: $scope.showCount,
-                start: $routeParams.page*$scope.showCount,
-                q:$routeParams.q
+                start: $routeParams.page * $scope.showCount,
+                q: $routeParams.q
             },
             function(data) {
+                // 每条电影的数据
                 $scope.datas = data;
                 $scope.isLoading = false;
                 $scope.total = data.total;
                 $scope.page = Math.ceil($scope.total / $scope.showCount);
                 $scope.curPage = parseInt($routeParams.page) + 1;
                 $scope.curPage = $scope.curPage > $scope.page ?
-                					 $scope.page:$scope.curPage;
+                    $scope.page : $scope.curPage;
                 $scope.showCount = data.count;
+
+                //分页部分
+                $scope.pageConfig = { totalPage: $scope.page,showPageCount:$scope.showPageCount};
+                $scope.getPage = service.getPage($scope.pageConfig);
+                $scope.pageArr = $scope.getPage($scope.curPage);
+                
                 $scope.$apply();
-            })
+            });
     }
 ]);
--- app/service/service.js
+++ app/service/service.js
@@ -1,33 +1,48 @@
 /*
-* @Author: Administrator
-* @Date:   2017-01-25 19:44:31
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-01-26 11:38:38
-*/
+ * @Author: Administrator
+ * @Date:   2017-01-25 19:44:31
+ * @Last Modified by:   Living Jiang
+ * @Last Modified time: 2017-01-26 18:30:15
+ */
 
 'use strict';
-angular.module("Movie.Service",[])
-.service("service",["$window","$document",function($window,$document) {
-	this.url = "https://api.douban.com/v2/movie";
-	this.jsonp = function (url,data,callback) {
-      	// 处理data数据
-      	var concatStr = url.indexOf("?")>=0?"&":"?";
-      	for(var k in data) {
-      		concatStr += k + "=" + data[k] + "&";
-      	}
-      	// 生成随机函数名callback
-      	var fnName = "jsonp_fn_" + $window.Math.random().toString().replace(".","");
-      	// 请求的最终地址
-      	var src = url + concatStr +"callback=" + fnName;
-      	// window 上绑定callback函数
-      	$window[fnName] = function(data) {
-      		callback(data);
-      		$document[0].body.removeChild(newStript);
-      	}
-      	var newStript = $document[0].createElement('script');
-      	newStript.src = src;
-      	$document[0].body.appendChild(newStript);
-      }
-}]);
-
-
+angular.module("Movie.Service", [])
+    .service("service", ["$window", "$document", function($window, $document) {
+        this.url = "https://api.douban.com/v2/movie";
+        this.jsonp = function(url, data, callback) {
+            // 处理data数据
+            var concatStr = url.indexOf("?") >= 0 ? "&" : "?";
+            for (var k in data) {
+                concatStr += k + "=" + data[k] + "&";
+            }
+            // 生成随机函数名callback
+            var fnName = "jsonp_fn_" + $window.Math.random().toString().replace(".", "");
+            // 请求的最终地址
+            var src = url + concatStr + "callback=" + fnName;
+            // window 上绑定callback函数
+            $window[fnName] = function(data) {
+                callback(data);
+                $document[0].body.removeChild(newStript);
+            }
+            var newStript = $document[0].createElement('script');
+            newStript.src = src;
+            $document[0].body.appendChild(newStript);
+        }
+        this.getPage = function(config) {
+            return function(cur) {
+                var arr = [];
+                var total = config.totalPage;
+                var showCount = config.showPageCount;
+                var start = cur - parseInt(showCount / 2);
+                start = start <= 0 ? 1 : start;
+                var end = start + showCount;
+                end = end > total ? total + 1 : end;
+                start = end - showCount;
+                start = start <= 0 ? 1 : start;
+                for (var i = start; i < end; i++) {
+                    arr.push(i);
+                }
+                return arr;
+            }
+        }
+    }]);
