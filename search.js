4d2d73afc77e2e1bcd78aef22bb2c6fca966a398
Living Jiang <694771790@qq.com>
2017-01-26 21:59:03 +0800

    commit it

--- search.js
+++ search.js
@@ -2,14 +2,22 @@
 * @Author: Administrator
 * @Date:   2017-01-26 01:23:57
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-01-26 15:57:12
+* @Last Modified time: 2017-01-26 01:24:53
 */
 
 'use strict';
-angular.module('Search', []).controller('searchController',[
+angular.module('Search', [
+          'ngRoute'
+      ]).controller('searchController',[
           '$scope',
           '$route',
-          'AppConfig',
-          function($scope,$route,AppConfig) {
+          function($scope,$route) {
           	$scope.keyword = '';
+          	$scope.search = function() {
+          		if($scope.keyword=='') return;
+          		$route.updateParams({
+          			category:"search",
+          			q:$scope.keyword
+          		});
+          	}
           }]);
